519118775b978ff21c8359fd692d0ec9b2336ee9
ariz1985 <123880648+ariz1985@users.noreply.github.com>
2024-03-29 08:23:46 +0530

    Create my_sec.tf

---
+++ terraform/my_sec.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "my_sec" {
+  name        = "my_sec"
+  description = "Allow inbound traffic and all outbound traffic"
+ 
+   tags = {
+    Name = "allow_tls"
+      }
+    egress{
+      from_port=0
+      to_port=0
+      protocol="-1"
+      cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+      from_port=80
+      to_port=80
+      protocol="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+      from_port=22
+      to_port=22
+      protocol="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+      from_port=443
+      to_port=443
+      protocol="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    }
+}
+      
