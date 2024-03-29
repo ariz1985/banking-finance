5a829db8a07a835869dfada1adf2ea91276202a8
ariz1985 <123880648+ariz1985@users.noreply.github.com>
2024-03-29 11:38:24 +0530

    Update my_sec.tf

--- terraform/my_sec.tf
+++ terraform/my_sec.tf
@@ -29,5 +29,11 @@ resource "aws_security_group" "my_sec" {
       protocol="tcp"
       cidr_blocks=["0.0.0.0/0"]
     }
+  ingress{
+      from_port=8080
+      to_port=8080
+      protocol="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    }
 }
       
