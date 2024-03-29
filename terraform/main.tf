5bd6b8fcc5ecd098f4c2ba52b2741954d608c442
ariz1985 <123880648+ariz1985@users.noreply.github.com>
2024-03-29 09:55:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-banking-server" {
   ami= "ami-080e1f13689e07408"
   instance_type = "t2.micro"
   vpc_security_groups_id=[aws_security_groups.my_sec.id]
-  key_name=aws
+  key_name="aws"
   tags = {
     Name = "my-banking-server"
   }
